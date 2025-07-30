95efea5cb6e6052aa00e9e201bbabc656a293d33
Mohamed Suhail Khan <141369674+MohamedSuhailKhan@users.noreply.github.com>
2025-07-31 00:38:48 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,13 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
+import react from '@vitejs/plugin-react'
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  server: {
-    host: "::",
-    port: 8080,
+export default defineConfig({
+  // Add this base property, using your repo name
+  base: '/bokaap-deli-reserve-bite/',
+  plugins: [react()],
+})
   },
   plugins: [
     react(),
